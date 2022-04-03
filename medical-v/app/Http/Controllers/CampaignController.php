afbe2b5ad64dd854cdfbf685f3e15f5fc5deb550
benediiktus <benediktusrafaelbagas@gmail.com>
2022-04-03 23:28:11 +0700

    Setup #1

--- medical-v/app/Http/Controllers/CampaignController.php
+++ medical-v/app/Http/Controllers/CampaignController.php
@@ -7,4 +7,7 @@ use Illuminate\Http\Request;
 class CampaignController extends Controller
 {
     //
+    public function index(){
+        return view('organisasi.addcampaign');
+    }
 }
---
+++ medical-v/resources/views/organisasi/addcampaign.blade.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Add Campaign</title>
+</head>
+<body>
+    <h1>Halo Dunia!</h1>
+
+    <form action="" method="post">
+        <label for="nama_campaign">Nama Campaign</label>
+        <input type="text" name="nama_campaign">
+
+        <label for="nama_org">Penyelenggara</label>
+        <input type="text" name="nama_org">
+
+        <label for="kategori">Kategori</label>
+        <input type="text" name="kategori">
+
+        <label for="tgl_mulai">Mulai Campaign</label>
+        <input type="date" name="tgl_mulai">
+    </form>
+</body>
+</html>
