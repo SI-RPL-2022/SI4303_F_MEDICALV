c932fd38611c52f4e23a38bd2a54a73392c9c02c
maulanadrif <maulanadaffarifki@gmail.com>
2022-04-06 00:00:54 +0700

    Update Header & Footer

--- medical-v/resources/views/includes/footer.blade.php
+++ medical-v/resources/views/includes/footer.blade.php
@@ -1,40 +1,5 @@
 <!-- Footer -->
-<footer class="text-center text-lg-start bg-light text-muted mt-5">
-    <!-- Section: Social media -->
-    <section
-        class="d-flex justify-content-center justify-content-lg-between p-4 border-bottom"
-    >
-        <!-- Left -->
-        <div class="me-5 d-none d-lg-block">
-            <span>Get connected with us on social networks:</span>
-        </div>
-        <!-- Left -->
-
-        <!-- Right -->
-        <div>
-            <a href="" class="me-4 text-reset">
-            <i class="fab fa-facebook-f"></i>
-            </a>
-            <a href="" class="me-4 text-reset">
-            <i class="fab fa-twitter"></i>
-            </a>
-            <a href="" class="me-4 text-reset">
-            <i class="fab fa-google"></i>
-            </a>
-            <a href="" class="me-4 text-reset">
-            <i class="fab fa-instagram"></i>
-            </a>
-            <a href="" class="me-4 text-reset">
-            <i class="fab fa-linkedin"></i>
-            </a>
-            <a href="" class="me-4 text-reset">
-            <i class="fab fa-github"></i>
-            </a>
-        </div>
-        <!-- Right -->
-        </section>
-        <!-- Section: Social media -->
-
+<footer class="text-center text-lg-start bg-light mt-5" style="background: linear-gradient(90deg, rgba(106, 185, 158, 0.83) 0%, rgba(203, 244, 231, 0.94) 110.24%);">
     <!-- Section: Links  -->
     <section class="">
         <div class="container text-center text-md-start mt-5">
@@ -43,10 +8,8 @@
             <!-- Grid column -->
             <div class="col-md-3 col-lg-4 col-xl-3 mx-auto mb-4">
                 <!-- Content -->
-                <h6 class="text-uppercase fw-bold mb-4">
-                <i class="fas fa-gem me-3"></i>Medical-V
-                </h6>
-                <p>
+                <h6 class="text-uppercase fw-bold mb-4" style="color: white">Medical-V</h6>
+                <p style="color: white">
                     Medical-V adalah aplikasi berbasis web yang membantu memberikan 
                     layanan kesehatan bagi masyarakat terpencil dengan mengumpulkan 
                     dan mengirimkan volunteer yang bekerja sama dengan mitra kesehatan 
@@ -58,40 +21,37 @@
             <!-- Grid column -->
             <div class="col-md-2 col-lg-2 col-xl-2 mx-auto mb-4">
                 <!-- Links -->
-                <h6 class="text-uppercase fw-bold mb-4">
+                <h6 class="text-uppercase fw-bold mb-4" style="color: white">
                 Fitur
                 </h6>
-                <p>
+                <p style="color: white">
                 <a href="#!" class="text-reset">Telusuri Campaign</a>
                 </p>
-                <p>
+                <p style="color: white">
                 <a href="#!" class="text-reset">Tentang Kami</a>
                 </p>
-                <p>
+                <p style="color: white">
                 <a href="#!" class="text-reset">Cara Kerja</a>
                 </p>
-                <p>
+                <p style="color: white">
                 <a href="#!" class="text-reset">Login</a>
                 </p>
-                <p>
+                <p style="color: white">
                 <a href="#!" class="text-reset">Daftar</a>
                 </p>
             </div>
             <!-- Grid column -->
     
             <!-- Grid column -->
-            <div class="col-md-4 col-lg-3 col-xl-3 mx-auto mb-md-0 mb-4">
+            <div class="col-md-4 col-lg-3 col-xl-3 mx-auto mb-md-0 mb-4" style="color: white">
                 <!-- Links -->
                 <h6 class="text-uppercase fw-bold mb-4">
                 Contact
                 </h6>
-                <p><i class="fas fa-home me-3"></i> JL. Telekomunikasi No. 1 Dayeuhkolot</p>
-                <p>
-                <i class="fas fa-envelope me-3"></i>
-                medicalv@gmail.com
-                </p>
-                <p><i class="fas fa-phone me-3"></i> + 6282775461894</p>
-                <p><i class="fas fa-print me-3"></i> + 6282339461748</p>
+                <p>JL. Telekomunikasi No. 1 Dayeuhkolot</p>
+                <p>medicalv@gmail.com</p>
+                <p>+62 82775461894</p>
+                <p>+62 82339461748</p>
             </div>
             <!-- Grid column -->
             </div>
@@ -101,7 +61,7 @@
     <!-- Section: Links  -->
 
     <!-- Copyright -->
-    <div class="text-center p-4" style="background-color: rgba(0, 0, 0, 0.05);">
+    <div class="text-center p-4" style="background-color: rgba(0, 0, 0, 0.05); color: white">
         © 2022 Copyright:
         <a class="text-reset fw-bold" href="#">Medical-V</a>
     </div>
--- medical-v/resources/views/includes/header.blade.php
+++ medical-v/resources/views/includes/header.blade.php
@@ -1,24 +1,24 @@
-<nav class="navbar navbar-expand-lg navbar-light bg-light">
-    <div class="container-fluid">
-        <a class="navbar-brand" href="#"><b>Medical-V</b></a>
+<nav class="navbar navbar-expand-lg" style="background: white">
+    <div class="container-fluid ms-5">
+        <a class="navbar-brand" href="#" style="color:#6AB99E;"><b>Medical-V</b></a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
             <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                 <li class="nav-item">
-                    <a class="nav-link active" aria-current="page" href="#">Telusuri Campaign</a>
+                    <a class="nav-link" href="#" style="color: black">Telusuri Campaign</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="#">Tentang Kami</a>
+                    <a class="nav-link" href="#" style="color: black">Tentang Kami</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="#">Cara Kerja</a>
+                    <a class="nav-link" href="#" style="color: black">Cara Kerja</a>
                 </li>
             </ul>
-            <ul class="navbar-nav">
+            <ul class="navbar-nav me-5">
                 <li class="nav-item">
-                    <a class="nav-link" href="#">Campaign Saya</a>
+                    <a class="nav-link" href="#" style="color: black">Campaign Saya</a>
                 </li>
             </ul>
         </div>
