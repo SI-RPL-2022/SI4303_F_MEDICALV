ba7e823bea8674f67d89e50f357da68387e65cc6
maulanadrif <maulanadaffarifki@gmail.com>
2022-04-06 11:53:46 +0700

    Update 2 Header & Footer

--- medical-v/resources/views/includes/header.blade.php
+++ medical-v/resources/views/includes/header.blade.php
@@ -1,24 +1,24 @@
 <nav class="navbar navbar-expand-lg" style="background: white">
     <div class="container-fluid ms-5">
-        <a class="navbar-brand" href="#" style="color:#6AB99E;"><b>Medical-V</b></a>
+        <a class="navbar-brand medical-v-color-text posisi-nav-logo" href="#"><img src="{{ asset('assets/img/logo/Logo-Medical-V.png') }}" alt="logo-navbar" width="120"></a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
             <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                 <li class="nav-item">
-                    <a class="nav-link" href="#" style="color: black">Telusuri Campaign</a>
+                    <a class="nav-link font-nav posisi-nav-telusuri mt-2" href="#" style="color: black">Telusuri Campaign</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="#" style="color: black">Tentang Kami</a>
+                    <a class="nav-link font-nav posisi-nav-tentang mt-2" href="#" style="color: black">Tentang Kami</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="#" style="color: black">Cara Kerja</a>
+                    <a class="nav-link font-nav posisi-nav-cara mt-2" href="#" style="color: black; width: 100%">Cara Kerja</a>
                 </li>
             </ul>
             <ul class="navbar-nav me-5">
                 <li class="nav-item">
-                    <a class="nav-link" href="#" style="color: black">Campaign Saya</a>
+                    <a class="nav-link font-nav posisi-nav-campaign mt-2 inline" href="#" style="color: black">Campaign Saya</a>
                 </li>
             </ul>
         </div>
