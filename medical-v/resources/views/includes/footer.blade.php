14a939a0a2faa393bcd1034b477cc42e812b3ba9
maulanadrif <maulanadaffarifki@gmail.com>
2022-04-09 17:38:09 +0700

    Update 3 Header & Footer

--- medical-v/resources/views/includes/footer.blade.php
+++ medical-v/resources/views/includes/footer.blade.php
@@ -1,5 +1,5 @@
 <!-- Footer -->
-<footer class="text-center text-lg-start bg-light mt-5" style="background: linear-gradient(90deg, rgba(106, 185, 158, 0.83) 0%, rgba(203, 244, 231, 0.94) 110.24%);">
+<footer class="text-center text-lg-start bg-light posisi-footer" style="background: linear-gradient(90deg, rgba(106, 185, 158, 0.83) 0%, rgba(203, 244, 231, 0.94) 110.24%);">
     <!-- Section: Links  -->
     <section class="">
         <div class="container text-center text-md-start mt-5">
@@ -8,7 +8,7 @@
             <!-- Grid column -->
             <div class="col-md-3 col-lg-4 col-xl-3 mx-auto mb-4">
                 <!-- Content -->
-                <h6 class="text-uppercase fw-bold mb-4" style="color: white">Medical-V</h6>
+                <h6 class="text-uppercase fw-bold mb-4" style="color: white"><i class="me-3"><img src="{{ asset('assets/img/logo/icon-logo-putih.png') }}" alt=""></i>Medical-V</h6>
                 <p style="color: white">
                     Medical-V adalah aplikasi berbasis web yang membantu memberikan 
                     layanan kesehatan bagi masyarakat terpencil dengan mengumpulkan 
@@ -58,13 +58,5 @@
             <!-- Grid row -->
         </div>
     </section>
-    <!-- Section: Links  -->
-
-    <!-- Copyright -->
-    <div class="text-center p-4" style="background-color: rgba(0, 0, 0, 0.05); color: white">
-        © 2022 Copyright:
-        <a class="text-reset fw-bold" href="#">Medical-V</a>
-    </div>
-    <!-- Copyright -->
 </footer>
 <!-- Footer -->
--- medical-v/resources/views/includes/header.blade.php
+++ medical-v/resources/views/includes/header.blade.php
@@ -13,12 +13,15 @@
                     <a class="nav-link font-nav posisi-nav-tentang mt-2" href="#" style="color: black">Tentang Kami</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link font-nav posisi-nav-cara mt-2" href="#" style="color: black; width: 100%">Cara Kerja</a>
+                    <a class="nav-link font-nav posisi-nav-cara mt-2" href="#" style="color: black; width: 50%">Cara Kerja</a>
                 </li>
             </ul>
             <ul class="navbar-nav me-5">
                 <li class="nav-item">
                     <a class="nav-link font-nav posisi-nav-campaign mt-2 inline" href="#" style="color: black">Campaign Saya</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link posisi-profile-header" href="#" style="color: black"><img src="{{ asset('assets/img/icon/profile.png') }}" alt="icon-profile"></a>
                 </li>
             </ul>
         </div>
